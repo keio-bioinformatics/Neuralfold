c50087fe877f6cf36edf3e81edd729b202acaaaa
秋山真那斗 <akiyama@akiyamatonoiMac.dna.bio.keio.ac.jp>
2017-08-17 21:14:43 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+import sys
+import SStruct
+
+args = sys.argv
+filename = arg[2]
+
+if args[1]=='train'
+
+    sstruct=SStruct.SStruct(filename)
+
+elif  args[1]=='test'
+
+else
