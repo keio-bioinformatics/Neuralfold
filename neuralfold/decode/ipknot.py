35c674ea9995d159c3b95579de261128c9fcdd7e
Kengo Sato <satoken@bio.keio.ac.jp>
2018-11-15 19:26:50 +0900

    small fixes

--- neuralfold/decode/ipknot.py
+++ neuralfold/decode/ipknot.py
@@ -15,14 +15,14 @@ class IPknot(Decoder):
         '''
         gamma = self.gamma if gamma is None else gamma
         prob = pulp.LpProblem("IPknot", pulp.LpMaximize)
-        seqlen = len(bpp)
-        nlevels = len(gamma)
+        N = len(bpp)
+        K = len(gamma)
         bpp = bpp.data if isinstance(bpp, Variable) else bpp
 
         if allowed_bp is None:
-            allowed_bp = np.ones(bpp.shape, dtype=np.bool)
+            allowed_bp = np.ones_like(bpp, dtype=np.bool)
         elif isinstance(allowed_bp, list):
-            a = np.zeros(bpp.shape, dtype=np.bool)
+            a = np.zeros_like(bpp, dtype=np.bool)
             for i, j in allowed_bp:
                 a[i, j] = a[j, i] = True
             allowed_bp = a
@@ -30,25 +30,25 @@ class IPknot(Decoder):
         assert allowed_bp.shape == bpp.shape
 
         # accelerate by 'at most gamma+1' candidates for each base
-        if int(max(gamma))<seqlen:
+        if int(max(gamma))<N:
             top_n = int(max(gamma))
             bpp_tri = np.triu(bpp)
             bpp = bpp_tri + np.transpose(bpp_tri)
             sorted_bp_idx = np.argsort(bpp, axis=1)[:, ::-1]
-            enabled_bp = np.zeros(bpp.shape, dtype=np.bool)
-            for i in range(seqlen):
+            enabled_bp = np.zeros_like(bpp, dtype=np.bool)
+            for i in range(N):
                 th_i = bpp[i, sorted_bp_idx[i, top_n]]
                 enabled_bp[i, bpp[i, :]>=th_i] = True
             allowed_bp &= enabled_bp & np.transpose(enabled_bp)
 
         # variables and objective function
-        x = [[[None for i in range(seqlen)] for j in range(seqlen)] for k in range(nlevels)]
-        x_i = [[[] for i in range(seqlen)] for k in range(nlevels)]
-        x_j = [[[] for j in range(seqlen)] for k in range(nlevels)]        
+        x = [[[None for i in range(N)] for j in range(N)] for k in range(K)]
+        x_i = [[[] for j in range(N)] for k in range(K)]
+        x_j = [[[] for i in range(N)] for k in range(K)]        
         obj = []
         th = 0 if disable_th else 1
-        for k in range(nlevels):
-            for j in range(seqlen):
+        for k in range(K):
+            for j in range(N):
                 for i in range(j):
                     if allowed_bp[i, j]:
                         s = (gamma[k]+1) * bpp[i, j] - th
@@ -57,45 +57,43 @@ class IPknot(Decoder):
                                 s += margin[i, j]
                             x[k][i][j] = x[k][j][i] = pulp.LpVariable('x[%d][%d][%d]' % (k, i, j), 0, 1, 'Binary')
                             obj.append(s * x[k][i][j])
-                            x_j[k][j].append(i)
-                            x_i[k][i].append(j)
                             x_i[k][j].append(i)
+                            x_j[k][i].append(j)
         prob += pulp.lpSum(obj)
 
         # constraints 1
-        for i in range(seqlen):
-            prob += pulp.lpSum([x[k][i][j] for j in x_i[k][i] for k in range(nlevels)]) <= 1
+        for i in range(N):
+            prob += pulp.lpSum([x[k][i][j] for j in x_i[k][i]+x_j[k][i] for k in range(K)]) <= 1
 
         # constraints 2
-        for k in range(nlevels):
-            x_kj = x_j[k]
-            for j2 in range(seqlen):
+        for k in range(K):
+            for j2 in range(N):
                 for j1 in range(j2):
-                    for i2 in x_kj[j2]:
+                    for i2 in x_i[k][j2]:
                         assert x[k][i2][j2] is not None
                         if i2 < j1:
-                            for i1 in x_kj[j1]:
+                            for i1 in x_i[k][j1]:
                                 if i1 < i2:
                                     assert x[k][i1][j1] is not None
                                     prob += pulp.lpSum(x[k][i1][j1] + x[k][i2][j2]) <= 1
 
         # constraints 3
-        for k2 in range(nlevels):
-            for j2 in range(seqlen):
-                for i2 in x_j[k2][j2]:
+        for k2 in range(K):
+            for j2 in range(N):
+                for i2 in x_i[k2][j2]:
                     assert x[k2][i2][j2] is not None
                     for k1 in range(k2):
-                        c1 = [x[k1][i1][j1] for i1 in x_i[k1][j1] for j1 in range(i2-1) if i1 > i2 and i1 < j2]
-                        c2 = [x[k1][i1][j1] for i1 in x_i[k1][j1] for j1 in range(j2+1, seqlen) if i1 > i2 and i1 < j2]
+                        c1 = [x[k1][i1][j1] for j1 in x_j[k1][i1] for i1 in range(0, i2-1) if i2 < j1 and j1 < j2]
+                        c2 = [x[k1][i1][j1] for i1 in x_i[k1][j1] for j1 in range(j2+1, N) if i2 < i1 and i1 < j2]
                         prob += pulp.lpSum(c1+c2) >= x[k2][i2][j2]
 
         # solve the IP problem
         prob.solve()
 
-        pair = [ [] for _ in range(nlevels) ]
-        for k in range(nlevels):
-            for j in range(seqlen):
-                for i in x_j[k][j]:
+        pair = [ [] for _ in range(K) ]
+        for k in range(K):
+            for j in range(N):
+                for i in x_i[k][j]:
                     assert x[k][i][j] is not None
                     if x[k][i][j].value() > 0.5:
                         pair[k].append((i,j))
@@ -105,12 +103,12 @@ class IPknot(Decoder):
     def calc_score(self, bpp, pair, gamma=None, margin=None):
         gamma = self.gamma if gamma is None else gamma
         s = np.zeros((1,1), dtype=np.float32)
-        if type(bpp) is Variable:
+        if isinstance(bpp, Variable):
             s = Variable(s)
 
         if len(pair) == 0:
             return s.reshape(1,)
-        elif len(gamma) == len(pair) and type(pair[0]) is list:
+        elif len(gamma) == len(pair) and isinstance(pair[0], list):
             pass
         else:
             pair = self.decode(bpp, gamma, margin=margin, allowed_bp=pair, disable_th=True)    
